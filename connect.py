ac318376cac10b1cf855d7b6f171669ec2b165c8
autchadaporn <inging.510801@gmail.com>
2020-09-02 12:35:31 +0700

    ..

--- connect.py
+++ connect.py
@@ -18,11 +18,9 @@ app.config['MYSQL_DB']='lalin_new'
 app.config['MYSQL_CURSORCLASS']='DictCursor'
 mysql = MySQL(app)
 
-
-
 @app.route('/')
 def index():
-    return render_template('test1.html')
+    return render_template('index.html')
 
 @app.route('/subject')
 def student():
@@ -34,11 +32,11 @@ def student():
 @app.route('/gradeall')
 def gradeall():
     cur = mysql.connection.cursor()
-    cur.execute("SELECT student_grade.student_id , subject.subject_id , subject.subject_nameTh , subject.subject_nameEng ,student_grade.grade , student_grade.unit , student_grade.term , student_grade.year FROM student_grade JOIN subject ON subject.subject_id = student_grade.subject_id WHERE student_id = '60023179' ") # ex. ดูว่ารหัสนิสิต 60023179 เรียนอะไรไปแล้วบ้าง
+    cur.execute("SELECT student_grade.student_id , subject.subject_id , subject.subject_nameTh , subject.subject_nameEng ,student_grade.grade , student_grade.unit , student_grade.term , student_grade.year FROM student_grade JOIN subject ON subject.subject_id = student_grade.subject_id WHERE student_id = '60020671' ") # ex. ดูว่ารหัสนิสิต 60023179 เรียนอะไรไปแล้วบ้าง
     data1=cur.fetchall()
     
     #------------------------------ start คำนวนเกรด gpax---------------------------------#
-    cur.execute("SELECT unit FROM student_grade WHERE student_id = '60023179' ")
+    cur.execute("SELECT unit FROM student_grade WHERE student_id = '60020671' ")
     unit = cur.fetchall()
     sumUnit = 0 
     for indexUnit in range(0,len(unit)):
@@ -47,7 +45,7 @@ def gradeall():
         sumUnit = sumUnit + float(unitCal) 
     # print(sumnUit)
 
-    cur.execute("SELECT grade FROM student_grade WHERE student_id = '60023179' ")
+    cur.execute("SELECT grade FROM student_grade WHERE student_id = '60020671' ")
     grade = cur.fetchall()
     #print(grade)
     sum = 0
@@ -75,7 +73,7 @@ def gradeall():
 
 @app.route('/showgrade')
 def showgrade():
-    """
+   
     cur = mysql.connection.cursor()
     cur.execute("SELECT MAX(year) FROM student_grade") # หาปีที่มากสุดใน database
     maxyear = cur.fetchall() # แสดงผล {'MAX(year)': '2'}
@@ -83,16 +81,17 @@ def showgrade():
     y = int(y)
     for i in range(1,y+1):
         for p in range(1,3): 
-            print(i,p)
-    """
-    # ---------  start เอาเกรด database แสดงผลหน้า showgrade.html ----------------#
+            print(i,p) # i คือปี p คือ เทอม
+    
+    #---------  start เอาเกรด database แสดงผลหน้า showgrade.html ----------------#
     cur = mysql.connection.cursor()
-    cur.execute("SELECT student_grade.student_id , subject.subject_id , subject.subject_nameTh , subject.subject_nameEng ,student_grade.grade , student_grade.unit , student_grade.term , student_grade.year FROM student_grade JOIN subject ON subject.subject_id = student_grade.subject_id WHERE student_grade.student_id = '60023179' and student_grade.year = '2' and student_grade.term = '1' ") # ex. ดูว่ารหัสนิสิต 60023179 เรียนอะไรไปแล้วบ้าง
+    cur.execute("SELECT student_grade.student_id , subject.subject_id , subject.subject_nameTh , subject.subject_nameEng ,student_grade.grade , student_grade.unit , student_grade.term , student_grade.year FROM student_grade JOIN subject ON subject.subject_id = student_grade.subject_id WHERE student_grade.student_id = '60023179' and student_grade.year = '"+i+"' and student_grade.term = '"+p+"' ") # ex. ดูว่ารหัสนิสิต 60023179 เรียนอะไรไปแล้วบ้าง
     data = cur.fetchall()
-    # ---------  stop เอาเกรด database แสดงผลหน้า showgrade.html ----------------#
-
+    print(data)
+    #---------  stop เอาเกรด database แสดงผลหน้า showgrade.html ----------------#
+    
     #------------------------------ start คำนวนเกรด GPA ---------------------------------#
-    cur.execute("SELECT unit FROM student_grade WHERE student_id = '60023179' and year ='2'  and term = '1' ")
+    cur.execute("SELECT unit FROM student_grade WHERE student_id = '60023179' and year ='"+i+"'  and term = '"+p+"' ")
     unit = cur.fetchall()
     sumUnit = 0 
     for indexUnit in range(0,len(unit)):
@@ -101,7 +100,7 @@ def showgrade():
         sumUnit = sumUnit + float(unitCal) 
     # print(sumnUit)
 
-    cur.execute("SELECT grade FROM student_grade WHERE student_id = '60023179' and year ='2' and term = '1'  ")
+    cur.execute("SELECT grade FROM student_grade WHERE student_id = '60023179' and year ='"+i+"' and term = '"+p+"'  ")
     grade = cur.fetchall()
     #print(grade)
     sum = 0
@@ -122,45 +121,23 @@ def showgrade():
     for i in range(0,4):
         itemGPA = itemGPA + x[i]
     print(itemGPA)
-    #------ stop แสดงทศนิยม2ตำแหน่ง--------#
-
+    #------ stop แสดงทศนิยม2ตำแหน่ง--------
     #------------------------------ stop คำนวนเกรด GPA ---------------------------------#
 
 
     return render_template('showgrade.html',data=data,GPA=itemGPA)
+    # return render_template('showgrade.html',data=data)
 
-@app.route('/calculate' , methods=['GET','POST'])
+#-----------------เลือกเกรดมาแสดง--------------------------------------
+@app.route('/calculate')
 def calculate():
     cur = mysql.connection.cursor()
     cur.execute("SELECT * FROM subject")
     subject = cur.fetchall()
-    showsubject =""
-    if request.method == 'POST':
-        subject_id = request.form['subject_id']
-        print(subject_id)
-        cur.execute("SELECT subject.subject_id , subject.subject_nameTh , subject.subject_nameEng ,subject.unit FROM subject WHERE subject.subject_id = '"+subject_id+"' ")
-        showsubject=cur.fetchall()
-        print(showsubject)
-    return render_template('calculate.html',subject=subject,showsubject=showsubject)
+    return render_template('test1.html',subject=subject)
     
-# @app.route("/calculate", methods=("GET", "POST"))
-# def calculate():
-#     form = MyForm()
-#     if request.method == 'POST':
-#         if form.validate() == True:
-#             return redirect(url_for('foo'))
-#         else:
-#             # If method is POST and form failed to validate
-#             # Do something (flash message?)
-#             return render_template("calculate.html", form=form)
-
-#     elif request.method == 'GET':
-#         return render_template("calculate.html", form=form)
-    
-
-
-    
-
+ 
+#-------------------แสดงแผนการเรียนทั้งหมด------------------------
 @app.route('/showstudyplan')
 def showstudyplan():
     cur = mysql.connection.cursor()
@@ -181,6 +158,27 @@ def showstudyplan():
     cur.execute("SELECT study_plan.study_plan_row_id , subject.subject_id , subject.subject_nameTh , subject.subject_nameEng , study_plan.year , study_plan.term FROM subject JOIN study_plan ON subject.subject_id = study_plan.subject_id WHERE study_plan.plan_id = '60' and study_plan.year ='4' and study_plan.term='2'")
     data8 = cur.fetchall()
     return render_template('showstudyplan.html',data1=data1,data2=data2,data3=data3,data4=data4,data5=data5,data6=data6,data7=data7,data8=data8)
+
+#---------------------ค้นหาแผนการศึกษา----------------------------------------
+@app.route("/searchplan",methods=['GET','POST'])
+def searchplan():
+    if request.method == 'POST':
+        year = request.form['year']
+        term = request.form['term']
+        # print(year,term)
+        cur = mysql.connection.cursor()
+        cur.execute("SELECT study_plan.study_plan_row_id , subject.subject_id , subject.subject_nameTh , subject.subject_nameEng, study_plan.year , study_plan.term FROM subject JOIN study_plan ON subject.subject_id = study_plan.subject_id WHERE study_plan.plan_id = '60' and study_plan.year ='"+year+"' and study_plan.term='"+term+"' ")
+        data = cur.fetchall()
+        # print(data)
+    return render_template('searchstudyplan.html',data = data,year=year,term=term)
+
+@app.route("/calendar")
+def  calendar():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT topic.topic_name, calendar.term ,calendar.date_start, calendar.date_stop,calendar.year FROM topic INNER JOIN calendar ON topic.topic_id = calendar.topic_id ")
+    data = cur.fetchall()
+    return render_template('calendar.html',data=data)
+    
 
 if __name__== "__main__" :
     app.run(debug=True)
